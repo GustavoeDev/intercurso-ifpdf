782edb320c9a222eae7110a362fdc765152e4915
Gustavo Emanuel <gustavoe.dev@gmail.com>
2024-12-01 16:06:00 -0300

    feat: adiciona responsividade na página de gerenciar-equipes

--- templates/static/student/js/manage-teams/table-min-max.js
+++ templates/static/student/js/manage-teams/table-min-max.js
@@ -1,15 +1,15 @@
-const minMaxButtons = document.querySelectorAll('.min-max-button')
+const minMaxButtons = document.querySelectorAll(".min-max-button");
 
 for (const button of minMaxButtons) {
-  button.addEventListener('click', () => {
-    let container = button.parentElement.parentElement.parentElement
-    console.log(container)
-    if (container.classList.contains('not-active')){
-      container.classList.remove('not-active')
-      button.classList.add('min')
+  button.addEventListener("click", () => {
+    let container = button.parentElement.parentElement.parentElement;
+    console.log(container);
+    if (container.classList.contains("not-active")) {
+      container.classList.remove("not-active");
+      button.classList.add("min");
     } else {
-      container.classList.add('not-active')
-      button.classList.remove('min')
+      container.classList.add("not-active");
+      button.classList.remove("min");
     }
-  })  
+  });
 }
