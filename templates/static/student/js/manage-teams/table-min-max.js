a8de09af419daac1348c863dcba6ef30abf12435
Gustavo Emanuel <gustavoe.dev@gmail.com>
2024-12-02 15:42:19 -0300

    refactor: melhora a lógica de abrir e fechar as tabelas de equipes

--- templates/static/student/js/manage-teams/table-min-max.js
+++ templates/static/student/js/manage-teams/table-min-max.js
@@ -1,15 +1,23 @@
 const minMaxButtons = document.querySelectorAll(".min-max-button");
 
-for (const button of minMaxButtons) {
-  button.addEventListener("click", () => {
-    let container = button.parentElement.parentElement.parentElement;
-    console.log(container);
-    if (container.classList.contains("not-active")) {
-      container.classList.remove("not-active");
-      button.classList.add("min");
-    } else {
-      container.classList.add("not-active");
-      button.classList.remove("min");
-    }
+minMaxButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const tableContainerClicked = btn.closest(".table-container");
+    const tableContainers = document.querySelectorAll(".table-container");
+
+    tableContainers.forEach((table) => {
+      const tableButton = table.querySelector(".min-max-button");
+
+      if (table === tableContainerClicked) {
+        table.classList.toggle("not-active");
+        tableButton.classList.toggle(
+          "min",
+          !table.classList.contains("not-active")
+        );
+      } else {
+        table.classList.add("not-active");
+        tableButton.classList.remove("min");
+      }
+    });
   });
-}
+});
