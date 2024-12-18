42f13bda597bfa491054a50fa2dc3706bab9a3a6
Gustavo Emanuel <gustavoe.dev@gmail.com>
2024-12-18 20:46:00 -0300

    feat: cria modal de excluir equipe da página de solicitações

--- templates/static/organizer/js/requests-page/create-modal.js
+++ templates/static/organizer/js/requests-page/create-modal.js
@@ -1,9 +1,22 @@
 const btnShowRequest = document.querySelectorAll(".show-request");
 
+// Aprovar equipe modal
+
 const approveTeamDialog = document.querySelector(".approve-team-dialog");
 const btnCloseApproveTeamDialog = document.querySelector(
   ".approve-team-dialog .dialog-header button"
 );
+
+// Excluir equipe modal
+
+const removeTeamDialog = document.querySelector(".remove-team-dialog");
+const btnCloseRemoveTeamDialog = document.querySelector(
+  ".remove-team-dialog .dialog-header button"
+);
+
+// Editar dados do participante modal
+
+// Excluir participante modal
 
 btnShowRequest.forEach((btn) => {
   btn.addEventListener("click", () => {
@@ -15,11 +28,16 @@ btnShowRequest.forEach((btn) => {
 
     if (typeRequestRelated === "aprovar equipe") {
       approveTeamDialog.showModal();
-      console.log("entrou");
+    } else if (typeRequestRelated === "excluir equipe") {
+      removeTeamDialog.showModal();
     }
   });
 });
 
 btnCloseApproveTeamDialog.addEventListener("click", () => {
   approveTeamDialog.close();
+});
+
+btnCloseRemoveTeamDialog.addEventListener("click", () => {
+  removeTeamDialog.close();
 });
