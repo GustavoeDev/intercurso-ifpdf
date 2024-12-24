46c04eb91136b2f9cead8225db2192720fba7acd
Hian Silva <hiansilacts@gmail.com>
2024-12-24 17:05:19 +0000

    feat: adiciona diálogo para editar placar na página de detalhes da competição

--- templates/static/organizer/js/datail-competition/create-modal.js
+++ templates/static/organizer/js/datail-competition/create-modal.js
@@ -1,7 +1,7 @@
-const endCompButton = document.querySelector("#end-comp-button") 
+const endCompButton = document.querySelector("#end-comp-button")
 const detailEndCompButton = document.querySelector("#datail-status-button")
 const endCompDialog = document.querySelector(".end-comp-dialog")
-const closeEndCompDialog = document.querySelector(".dialog-header button")
+const closeEndCompDialog = endCompDialog.querySelector(".dialog-header button")
 
 endCompButton.addEventListener('click', () => {
   endCompDialog.showModal()
@@ -13,4 +13,19 @@ detailEndCompButton.addEventListener('click', () => {
 
 closeEndCompDialog.addEventListener('click', () => {
   endCompDialog.close()
+})
+
+const cardsRoundContent = document.querySelectorAll(".card-round-container-content")
+const editScoreboardDialog = document.querySelector(".edit-scoreboard-dialog")
+const closeEditScoreboardDialog = editScoreboardDialog.querySelector(".edit-scoreboard-dialog-content .dialog-header")
+
+
+cardsRoundContent.forEach(card => {
+  card.addEventListener('click', () => {
+    editScoreboardDialog.showModal()
+  })
+});
+
+closeEditScoreboardDialog.addEventListener('click', () => {
+  editScoreboardDialog.close()
 })
