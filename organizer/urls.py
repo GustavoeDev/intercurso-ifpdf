4aada4a3b7e4c8a78ef355df77777150c046d883
Hian Silva <hiansilacts@gmail.com>
2024-12-26 14:36:07 -0300

    refactor: atualiza comentários nas URLs para incluir id/nome da competição

--- organizer/urls.py
+++ organizer/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('equipes/registrar-equipe', views.view_register_team, name="register-team"),
     path('equipes/editar-equipe', views.view_edit_team, name="edit-team"),
     path('competicoes/', views.view_competitions_page, name="competitions_list"),
-    path('competicoes/detail', views.view_detail_comp_page, name="detail"), # deatail vai ser subituido pelo id da competição
+    path('competicoes/detail', views.view_detail_comp_page, name="detail"), # deatail vai ser subituido pelo id/nome da competição
     path('solicitacoes/', views.view_requests, name="requests_list"),
 ]
