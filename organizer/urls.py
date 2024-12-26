790b2182d0f529ac4a7494fa3e80370da894f4e1
Hian Silva <hiansilacts@gmail.com>
2024-12-26 14:31:51 -0300

    refactor: atuiliza as urls do organizer

--- organizer/urls.py
+++ organizer/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('modalidades/', views.view_modalitypage, name="view_modalitypage"),
-    path('equipes/', views.view_teams_page, name="view_teams_page"),
+    path('modalidades/', views.view_modalitypage, name="modality_list"),
+    path('equipes/', views.view_teams_page, name="teams_list"),
     path('equipes/registrar-equipe', views.view_register_team, name="register-team"),
     path('equipes/editar-equipe', views.view_edit_team, name="edit-team"),
-    path('competicoes/', views.view_competitions_page, name="competitions_page"),
-    path('competicoes/detail', views.view_detail_comp_page, name="detail_competition"), # deatail vai ser subituido pelo id da competição
-    path('solicitacoes/', views.view_requests, name="view_requests"),
+    path('competicoes/', views.view_competitions_page, name="competitions_list"),
+    path('competicoes/detail', views.view_detail_comp_page, name="detail"), # deatail vai ser subituido pelo id da competição
+    path('solicitacoes/', views.view_requests, name="requests_list"),
 ]
