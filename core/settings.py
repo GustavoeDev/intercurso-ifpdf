888883371d146b399653c491b7201699bd1d27e4
Gustavo Emanuel <gustavoe.dev@gmail.com>
2024-12-26 17:51:06 -0300

    refactor: remove student and organizer apps and update related views, models, and templates

--- core/settings.py
+++ core/settings.py
@@ -40,8 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'competitions',
-    'student',
-    'organizer',
 ]
 
 MIDDLEWARE = [
--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('student.urls')),
     path('', include('users.urls')),
     path('', include('competitions.urls')),
-    path('organizador/', include('organizer.urls'))
 ]
