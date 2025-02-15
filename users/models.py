df3d8bca0a4d8ecd699f57e9d5557b0425b458ac
GustavoeDev <gustavoe.dev@gmail.com>
2025-02-15 13:33:54 -0300

    feat: add Course model and link to CustomUser

--- users/models.py
+++ users/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+class Course(models.Model):
+   name = models.CharField(max_length=200)
+
 class CustomUser(AbstractUser):
    registration = models.IntegerField(unique=True)
    name = models.CharField(max_length=200)
    email = models.EmailField(unique=True)
+   course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True, blank=True)
 
    USERNAME_FIELD = 'registration'
    REQUIRED_FIELDS = ['email', 'name']
