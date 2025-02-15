bbcb5cc8e97056995aa4ca3d90dd0e988f5f070b
GustavoeDev <gustavoe.dev@gmail.com>
2025-02-15 12:45:08 -0300

    feat: implement custom user model with additional fields

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+   registration = models.IntegerField(unique=True)
+   name = models.CharField(max_length=200)
+   email = models.EmailField(unique=True)
+
+   USERNAME_FIELD = 'registration'
+   REQUIRED_FIELDS = ['email', 'name']
+
+   def __str__(self):
+       return self.name
