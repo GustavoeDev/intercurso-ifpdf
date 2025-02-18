9e9a1cf5831b3bc14a46a23e2b03fb33d8b2de29
GustavoeDev <gustavoe.dev@gmail.com>
2025-02-18 15:55:22 -0300

    feat: register Team, Competition, and Modality models in admin; update manage_teams template to display team members dynamically

--- competitions/admin.py
+++ competitions/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Team, Competition, Modality
 
-# Register your models here.
+admin.site.register(Team)
+admin.site.register(Competition)
+admin.site.register(Modality)
