c214205ce256a2862db22557d714241b4c186242
GustavoeDev <gustavoe.dev@gmail.com>
2025-02-22 09:37:57 -0300

    feat: replace LoginView with CustomLoginView to handle success URL based on user group

--- users/views.py
+++ users/views.py
@@ -9,10 +9,15 @@ from django.shortcuts import render
 from django.contrib.auth import logout
 from django.shortcuts import redirect
 
-class LoginView(LoginView):
-   template_name = 'login_page.html'
-   redirect_authenticated_user = True
-   authentication_form = LoginForm
+class CustomLoginView(LoginView):
+    template_name = 'login_page.html' 
+    redirect_authenticated_user = True
+    authentication_form = LoginForm
+    
+    def get_success_url(self):
+        if self.request.user.groups.filter(name='Organizer').exists():
+            return '/organizador/modalidades/' 
+        return '/' 
 
 def LogoutView(request):
    logout(request)
